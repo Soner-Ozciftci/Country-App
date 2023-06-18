0c9ae33c99c8f90c2e2567c06a4a062c47a1d242
Soner <ozciftci@gmail.com>
2023-06-18 16:23:36 +0200

    last position

--- app.js
+++ app.js
@@ -1,114 +1,102 @@
-//? api linki :  https://restcountries.com/v3.1/all
-
-//? Tum sayfa yuklendiginde getData() fonk calistirarak  API'deki datayi indir ve  ulke isimlerini select'e bas
-
-//? 1.yazim sekli
-
-window.addEventListener("load", () => {
-  getData();
-});
-//? 2.yazim sekli
-// window.onload = function () {
-//   getData();
-
-// }
-let ülkeler; //?datayi globale cekmek icin
+let countries;
 
 const getData = async () => {
   try {
     const URL = "https://restcountries.com/v3.1/all";
     const res = await fetch(URL);
     if (!res.ok) {
-      throw new Error();
+      throw new Error("hatasız kul olmaz 😅");
     }
     const data = await res.json();
-    ülkeler = data; //?datayi countries e aktararak  globale cektim
-    console.log(ülkeler);
-    nameSelect(data);
-
-    // console.log(data);
-  } catch (error) {
-    console.log(error);
+    countries = data;
+    getName(data);
+  } catch (err) {
+    console.log(err);
+    // cardSection.innerHTML = `
+    // <img src="./img/404.png" class="card-img-top" alt="..." />`;
   }
 };
-
-const select = document.querySelector(".form-select"); //?event dada kullanmak icin disarda tanimladim
-//?select'te  ulke ismi degistiginde secilen ulkenin bilgilerini
-//? Card olarak DOM'a bas
-
-const nameSelect = (data) => {
-  ülkeler.forEach((element) => {
-    // console.log(element);
-
-    select.innerHTML += `<option value=${element.name.common}>${element.name.common}</option>`;
-  });
-  // domaYaz(data)
-};
+const select = document.querySelector(".form-select");
+// console.log(select);
 
 select.addEventListener("change", (e) => {
-  const selected = e.currentTarget.value; //?currenttarget select 'i isaret eder
+  const selected = e.currentTarget.value;
   console.log(selected);
   if (selected) {
-    // console.log(ülkeler);
-    const secilenUlke = ülkeler.filter((item) => {
-      return item.name.common === selected;
+    const selectedCountry = countries.filter((c) => {
+      return c.name.common === selected;
     });
-    console.log(secilenUlke);
-    domaYaz(secilenUlke[0]);
+    console.log(selectedCountry);
+    if (selectedCountry.length > 0) {
+      writeDOM(selectedCountry);
+    } else {
+      console.log("Selected country not found");
+    }
   }
 });
-
-const domaYaz = (y) => {
+const getName = (arr) => {
+  arr.map((item) => {
+    const cName = item.name.common;
+    // console.log(cName);
+    select.innerHTML += `
+    <option value=${cName}>${cName}</option>
+    `;
+  });
+};
+const writeDOM = (w) => {
   const {
-    flags: { png }, //?nested objectlerde destructuring
-    name: { common }, //?nested objectlerde destructuring
+    flags: { png },
+    name: { common },
     region,
     capital,
     languages,
     currencies,
     population,
     borders,
-    maps: { googleMaps }, //?nested objectlerde destructuring
-  } = y;
+    maps: { googleMaps },
+  } = w[0];
 
-  const cardSection = document.getElementById("cards");
-  cardSection.innerHTML = `<div class="card m-auto mt-4" style="width: 28rem">
-              <img src="${png}" class="card-img-top" alt="..." />
-              <div class="card-body">
-                <h5 class="card-title fs-2">${common}</h5>
-              </div>
-                  <ul class="list-group list-group-flush">
-              <li class="list-group-item">
-                <i class="fa-solid fa-earth-oceania"></i><span class="fw-bold"> Region:</span> ${region}
-              </li>
-              <li class="list-group-item">
-                <i class="fas fa-lg fa-landmark"></i>
-                <span class="fw-bold"> Capitals:</span> ${capital}
-              </li>
-              <li class="list-group-item">
-                <i class="fas fa-lg fa-comments"></i>
-                <span class="fw-bold"> Languages:</span>${Object.values(
-                  languages
-                )}
-              </li>
-              <li class="list-group-item">
-                <i class="fas fa-lg fa-money-bill-wave"></i>
-                <span class="fw-bold"> Currencies:</span> ${
-                  Object.values(currencies)[0].name //?Object.values() metodunun ciktisi array dir
-                }, ${Object.values(currencies)[0].symbol}
-              </li>
-              <li class="list-group-item">
-              <i class="fa-solid fa-people-group"></i></i>
-              <span class="fw-bold"> Population:</span> ${population}
-            </li>
-              <li class="list-group-item">
-              <i class="fa-sharp fa-solid fa-road-barrier"></i>
-              <span class="fw-bold"> Borders:</span> ${borders}
-            </li>
-            </li>
-            <li class="list-group-item">
-              <i class="fa-solid fa-map-location-dot"></i><span class="fw-bold"> Map:</span> <a href=${googleMaps}
-                 target='_blank'> Go to google map</a> </li>
-            </ul>
-        </div>`;
+  const cardSection = document.getElementById("card");
+  cardSection.innerHTML = `
+  <div class="card m-auto mt-4" style="width: 28rem">
+        <img src="${png}" class="card-img-top" alt="..." />
+        <div class="card-body">
+          <h5 class="card-title fs-2">${common}</h5>
+        </div>
+        <ul class="list-group list-group-flush">
+    <li class="list-group-item">
+      <i class="fa-solid fa-earth-oceania"></i><span class="fw-bold"> Region:</span> ${region}
+    </li>
+    <li class="list-group-item">
+      <i class="fas fa-lg fa-landmark"></i>
+      <span class="fw-bold"> Capitals:</span> ${capital}
+    </li>
+    <li class="list-group-item">
+      <i class="fas fa-lg fa-comments"></i>
+      <span class="fw-bold"> Languages:</span>${Object.values(languages)}
+    </li>
+    <li class="list-group-item">
+      <i class="fas fa-lg fa-money-bill-wave"></i>
+      <span class="fw-bold"> Currencies:</span> 
+      ${Object.values(currencies)[0].name}, 
+      ${Object.values(currencies)[0].symbol}
+    </li>
+    <li class="list-group-item">
+    <i class="fa-solid fa-people-group"></i></i>
+    <span class="fw-bold"> Population:</span> ${population}
+  </li>
+    <li class="list-group-item">
+    <i class="fa-sharp fa-solid fa-road-barrier"></i>
+    <span class="fw-bold"> Borders:</span> ${borders}
+  </li>
+  </li>
+  <li class="list-group-item">
+    <i class="fa-solid fa-map-location-dot"></i><span class="fw-bold"> Map:</span> <a href= ${googleMaps}
+       target='_blank'> Go to google map</a> </li>
+  </ul>
+      </div>`;
 };
+
+window.addEventListener("load", () => {
+  getData();
+});
